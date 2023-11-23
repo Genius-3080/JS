f93dc0b94926436f171810c73fd62ce4d7dbdb27
Genius-3080 <abdulboriytolqinov00200@gmail.com>
2023-11-23 12:26:52 +0500

    new js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+alert ('Welcome to site');
+var names = prompt('please write your name')
+var age = prompt('please write your age')
+var email = prompt('please write your email')
+console.log('name:' + names)
+console.log('age:' + age)
+console.log('email:' + email)
